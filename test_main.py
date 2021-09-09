88f0c610e78fd4058a9990e88efa62f65560cd98
EC2 Default User <ec2-user@ip-172-31-36-175.us-east-2.compute.internal>
2021-09-09 02:49:39 +0000

    updating test file

--- test_main.py
+++ test_main.py
@@ -6,3 +6,6 @@ def test_daysUntilNextHoliday():
     assert asyncio.run(daysUntilNextHoliday("2021-09-03")) == {
         "Days until next holiday": 3
     }
+    assert asyncio.run(daysUntilNextHoliday("2022-09-03")) == {
+        "Days until next holiday": "Oops! Date is not in 2021."
+    }
