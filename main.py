9d5e099d7cfd4a48bc49f8077992ff952fec4406
EC2 Default User <ec2-user@ip-172-31-36-175.us-east-2.compute.internal>
2021-09-20 07:29:20 +0000

    Updating main.py and test file

--- main.py
+++ main.py
@@ -7,31 +7,34 @@ from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
 
 app = FastAPI()
 
-dates = pd.DataFrame({"date": pd.date_range("2021-01-01", "2022-01-01")})
-cal = calendar()
-holidays = cal.holidays(start=dates["date"].min(), end=dates["date"].max())
 
 
 @app.get("/")
 async def root():
     return {
-        "message": "Please enter a date in 2021 with the format 'yyyy-mm-dd' to see how far the next holiday is!"
+        "message": "Please enter a date with the format 'yyyy-mm-dd' to see how far the next holiday is!"
     }
 
 
 @app.get("/HolidayTime/{date}")
+
 async def daysUntilNextHoliday(date: str):
     """Calculate Days Until Next U.S. Federal Holiday"""
     date = pd.to_datetime(date, format="%Y-%m-%d")
+    year = date.year
+    start = str(year)+"-01-01"
+    end = str(year+1)+"-01-01"
+    dates = pd.DataFrame({"date": pd.date_range(start, end)})
+    cal = calendar()
+    holidays = cal.holidays(start=dates["date"].min(), end=dates["date"].max())
+    
     difference = []
-    if (date > pd.to_datetime("2021-12-31", format="%Y-%m-%d")) or (
-        date < pd.to_datetime("2021-01-01", format="%Y-%m-%d")
-    ):
-        return {"Days until next holiday": "Oops! Date is not in 2021."}
-    else:
-        for day in holidays:
-            difference.append(int(str((day - date).days)))
-        return {"Days until next holiday": min([x for x in difference if x >= 0])}
+    
+    for day in holidays:
+        difference.append(int(str((day - date).days)))
+    
+    return {"Days until next U.S. Federal Holiday": min([x for x in difference if x >= 0])}
+
 
 
 if __name__ == "__main__":
--- test_main.py
+++ test_main.py
@@ -4,8 +4,8 @@ from main import daysUntilNextHoliday
 
 def test_daysUntilNextHoliday():
     assert asyncio.run(daysUntilNextHoliday("2021-09-03")) == {
-        "Days until next holiday": 3
+        "Days until next U.S. Federal Holiday": 3
     }
     assert asyncio.run(daysUntilNextHoliday("2022-09-03")) == {
-        "Days until next holiday": "Oops! Date is not in 2021."
+        "Days until next U.S. Federal Holiday": 2
     }
